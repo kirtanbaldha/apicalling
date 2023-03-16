9900d174dd488d23c91e9c55708abc35298ea63c
jay5260 <jaykumbhani4242@gmail.com>
2023-03-16 07:38:51 +0530

    16/3/23

--- apicalling/ViewController.swift
+++ apicalling/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  apicalling
-//
-//  Created by R & W on 16/03/23.
-//
-
 import UIKit
 
+struct Get:Codable {
+    var userId : Int
+    var id : Int
+    var title : String
+    var body : String
+    
+}
+
 class ViewController: UIViewController {
+    var arr : [Get] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
+       
     }
-
+    
+    func getData() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        var ur = URLRequest(url: url!)
+        ur.httpMethod = "GET"
+        URLSession.shared.dataTask(with: ur) { data, URLResponse, error in
+            do{
+                if error == nil{
+                    self.arr = try JSONDecoder().decode([Get].self, from: data!)
+                    print(self.arr)
+                }
+            }
+            catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 
 }
 
